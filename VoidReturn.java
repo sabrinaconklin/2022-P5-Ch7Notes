efabd073ceb4a71e59ea9f7429af318e1843096e
Sabrina <sabrina.conklin@vtsd.com>
2022-11-29 13:12:00 -0500

    jhmuh

--- VoidReturn.java
+++ VoidReturn.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
 public class VoidReturn {
+   public static void main(String args[]) {
+      int total = addNums();
 
-   public static void main(String args[]){
-       
-
+      System.out.println("Your total again was: " + total);
    }
 
+   public static int addNums(){
+
+      System.out.println("We are going to add two numbers");
+      System.out.println("Please enter them below");
+
+      Scanner scan = new Scanner(System.in);
+      int num1 = scan.nextInt();
+      int num2 = scan.nextInt();
+
+      return (num1 + num2);
+   }
 }
